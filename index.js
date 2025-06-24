9442622bf92c9d2e9ae05dc7c80541f7f2e3e118
Siddharth Chopra <contact.sid.chopra@gmail.com>
2025-06-24 16:51:02 +0530

    v-0.1

---
+++ index.js
@@ -0,0 +1,42 @@
+function scrollto(elemid) {
+    document.getElementById(elemid).scrollIntoView({ behavior: "smooth" });
+}
+
+function toggleNavbar(curr) {
+    if (curr == 'menu') {
+        document.getElementById('threelines').style.display = 'none';
+        document.getElementById('cross').style.display = 'block';
+        document.getElementById('mobilenavmenu').style.display = 'flex';
+    }
+    else if (curr == 'close') {
+        document.getElementById('threelines').style.display = 'block';
+        document.getElementById('cross').style.display = 'none';
+        document.getElementById('mobilenavmenu').style.display = 'none';
+    }
+}
+
+async function submit() {
+    let name = document.getElementById("nameinput").value;
+    let email = document.getElementById("emailinput").value;
+    let number = document.getElementById("numberinput").value;
+    let query = document.getElementById("querybox").value;
+
+    let json = {
+        name,
+        email,
+        number,
+        query
+    }
+
+    await fetch("https://formsubmit.co/contact.sid.chopra@gmail.com",
+        {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(json)
+        }
+    )
+}
+
+
