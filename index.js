521b5c2fac2389e7b7c669663510c048b9bd8620
Siddharth Chopra <contact.sid.chopra@gmail.com>
2025-07-18 20:41:18 +0530

    feat: use web3forms

--- index.js
+++ index.js
@@ -16,49 +16,49 @@ function toggleNavbar(curr) {
 }
 
 async function submit() {
-    let name = document.getElementById("nameinput").value;
-    let email = document.getElementById("emailinput").value;
-    let number = document.getElementById("numberinput").value;
-    let query = document.getElementById("querybox").value;
+    // let name = document.getElementById("nameinput").value;
+    // let email = document.getElementById("emailinput").value;
+    // let number = document.getElementById("numberinput").value;
+    // let query = document.getElementById("querybox").value;
     
-    document.getElementById('submit').disabled = true;
+    // document.getElementById('submit').disabled = true;
 
-    let json = {
-        name,
-        email,
-        number,
-        query
-    }
-    if (name.trim() === "" || email.trim() === "" || number.trim() === "" || query.trim() === ""){
-        document.getElementById('success').classList.add('hidden');
-        document.getElementById('failure').classList.add('hidden');
-        document.getElementById('incomplete').classList.remove('hidden');
-        document.getElementById("submit").onclick = submit;
-        return;
-    }
+    // let json = {
+    //     name,
+    //     email,
+    //     number,
+    //     query
+    // }
+    // if (name.trim() === "" || email.trim() === "" || number.trim() === "" || query.trim() === ""){
+    //     document.getElementById('success').classList.add('hidden');
+    //     document.getElementById('failure').classList.add('hidden');
+    //     document.getElementById('incomplete').classList.remove('hidden');
+    //     document.getElementById("submit").onclick = submit;
+    //     return;
+    // }
     
-    await fetch("/submit",
-        {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(json)
-        }
-    ).then((response)=>{
-        if (response.ok){
-            document.getElementById('success').classList.remove('hidden');
-            document.getElementById('failure').classList.add('hidden');
-            document.getElementById('incomplete').classList.add('hidden');
-        }
-        else{
-            document.getElementById('failure').classList.remove('hidden');
-            document.getElementById('success').classList.add('hidden');
-            document.getElementById('incomplete').classList.add('hidden');
+    // await fetch("/submit",
+    //     {
+    //         method: "POST",
+    //         headers: {
+    //             'Content-Type': 'application/json'
+    //         },
+    //         body: JSON.stringify(json)
+    //     }
+    // ).then((response)=>{
+    //     if (response.ok){
+    //         document.getElementById('success').classList.remove('hidden');
+    //         document.getElementById('failure').classList.add('hidden');
+    //         document.getElementById('incomplete').classList.add('hidden');
+    //     }
+    //     else{
+    //         document.getElementById('failure').classList.remove('hidden');
+    //         document.getElementById('success').classList.add('hidden');
+    //         document.getElementById('incomplete').classList.add('hidden');
 
-        }
-        document.getElementById("submit").onclick = submit;
-    })
+    //     }
+    //     document.getElementById("submit").onclick = submit;
+    // })
 
     
 
